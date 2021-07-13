948ccd0496134eccc4ae576090c52dcae404096b
Abhishek Swaminathan <44006049+abhisheksn@users.noreply.github.com>
2021-07-13 16:13:35 -0400

    Updated winner determination script
    Update the script to add the winner determination algorithm

--- game.py
+++ game.py
@@ -25,6 +25,17 @@ computer_choice = random.choice(valid_options)
 print("Computer chose:", computer_choice)
 
 #Determine the winner
+if user_input == computer_choice:
+# source: https://www.programiz.com/python-programming/input-output-import
+  result_display = ("Both players selected {}. The game is a tie.".format(user_input))
+elif user_input == "rock" and computer_choice == "scissors":
+  result_display = "Rock smashes scissors. Congrats! You won."
+elif user_input == "paper" and computer_choice == "rock":
+  result_display = "Paper covers rock. Congrats! You won."
+elif user_input == "scissors" and computer_choice == "paper":
+  result_display = "Scissors cuts paper. Congrats! You won."
+else:
+  result_display = "Oh no! The computer won. Try again?"
 
-
+print(result_display)
 #Display final results
