ec24071482a9e33d02d044ed82dbcbfc0db37fad
Abhishek Swaminathan <44006049+abhisheksn@users.noreply.github.com>
2021-07-13 16:04:07 -0400

    Script to capture user input
    Added script to capture user input.

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+#Ask for a User Input
+#source:
+user_input = input("Please choose either 'rock', 'paper', or 'scissors': ")
+print("You chose:", user_input)
+
+#Validate the User Input
+
+
+#Generate a Computer Choice
+
+
+#Determine the winner
+
+
+#Display final results
