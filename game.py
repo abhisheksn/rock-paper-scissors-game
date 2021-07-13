d1e94210968b034f1f001da6dbd2cd0a6006d74b
Abhishek Swaminathan <44006049+abhisheksn@users.noreply.github.com>
2021-07-13 16:08:52 -0400

    Added script for computer choice
    Updated game script for computer choice

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # game.py
+import random
 
 print("Rock, Paper, Scissors, Shoot!")
 
@@ -15,7 +16,13 @@ else:
     exit()
 
 #Generate a Computer Choice
+#source: https://www.w3schools.com/python/ref_random_choice.asp
 
+valid_options = ["rock", "paper", "scissors"]
+
+computer_choice = random.choice(valid_options)
+
+print("Computer chose:", computer_choice)
 
 #Determine the winner
 
