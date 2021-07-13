4ad788c73523670172b20fbc78bb052035fdd2f0
Abhishek Swaminathan <44006049+abhisheksn@users.noreply.github.com>
2021-07-13 16:24:59 -0400

    Updated Results display and script to install packages
    Updated python script to install packages. Created requirements.txt and updated the results display.

--- game.py
+++ game.py
@@ -1,30 +1,34 @@
 # game.py
+#source:https://www.w3schools.com/python/ref_random_choice.asp
 import random
+#source: code from https://github.com/prof-rossetti/my-first-python-app
+import os
+from dotenv import load_dotenv  # source:https://github.com/theskumar/python-dotenv
 
-print("Rock, Paper, Scissors, Shoot!")
+load_dotenv()  #invokes / uses the function we got from the third-party package. this one happens to read env vars from the ".env" file. see the "python-dotenv" package docs for more info
+
+# uses the os module to read the specified environment variable and store it in a corresponding python variable
+user_name = os.getenv("PLAYER_NAME", default="Player One")
 
 #Ask for a User Input
 #source:
 user_input = input("Please choose either 'rock', 'paper', or 'scissors': ")
-print("You chose:", user_input)
 
 #Validate the User Input
 if (user_input == "rock") or (user_input == "paper") or (user_input == "scissors"):
-    print("VALID")
+    print("Valid input.")
 else:
-    print("OOPS!, INVALID, Please try again!")
+    print("Oops! that's an invalid input. Please try again!")
     exit()
 
 #Generate a Computer Choice
 #source: https://www.w3schools.com/python/ref_random_choice.asp
 
 valid_options = ["rock", "paper", "scissors"]
-
 computer_choice = random.choice(valid_options)
 
-print("Computer chose:", computer_choice)
-
 #Determine the winner
+
 if user_input == computer_choice:
 # source: https://www.programiz.com/python-programming/input-output-import
   result_display = ("Both players selected {}. The game is a tie.".format(user_input))
@@ -37,5 +41,18 @@ elif user_input == "scissors" and computer_choice == "paper":
 else:
   result_display = "Oh no! The computer won. Try again?"
 
-print(result_display)
 #Display final results
+
+print("------------------")
+# source: https://www.programiz.com/python-programming/input-output-import
+print("Welcome {} to my Rock, Paper, Scissors game. Let's play!" .format(user_name))
+print("------------------")
+print("Rock, Paper, Scissors, Shoot!")
+print("------------------")
+print("You chose:", user_input)
+print("The computer chose:", computer_choice)
+print("------------------")
+print(result_display)
+print("------------------")
+# source: https://www.programiz.com/python-programming/input-output-import
+print("{}, thanks for playing. Please try again if you liked it.".format(user_name))
